d0c8a00a6826ea4cf7bb8541ac5c7102450036b6
Jonathan Rasmusson <rasmus4200@gmail.com>
2021-08-04 06:49:57 -0600

    test1

--- arcade.rb
+++ arcade.rb
@@ -1,5 +1,24 @@
 require 'sinatra'
+require 'json'
 
-get '/' do
-  'Hello world!'
+before do
+  content_type :json
+end
+
+get '/hi' do
+  "Hello World"
+end
+
+get '/game' do
+  {
+      game: "Wake me Up",
+      year: "1984"
+  }.to_json
+end
+
+get '/game/1' do
+  {
+      game: "Wake me Up",
+      year: "1984"
+  }.to_json
 end
