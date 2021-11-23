6e8a66787c5cc84bac8f11deaad04856ffb7d4ad
Jonathan Rasmusson <rasmus4200@gmail.com>
2021-11-23 09:04:49 -0700

    Account

--- bankey.rb
+++ bankey.rb
@@ -5,6 +5,15 @@ before do
   content_type :json
 end
 
+get '/bankey/account/1' do
+  {
+      id: "1",
+      accountType: "Banking",
+      accountName: "Savings",
+      balance: 100.00
+  }.to_json
+end
+
 get '/bankey/profile/1' do
   {
       id: "1",
