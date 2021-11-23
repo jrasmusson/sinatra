7d7c983e884c2a69c5dcf311e428d67e87397060
Jonathan Rasmusson <rasmus4200@gmail.com>
2021-11-23 09:02:23 -0700

    Fix

--- bankey.rb
+++ bankey.rb
@@ -5,15 +5,6 @@ before do
   content_type :json
 end
 
-# Accounts
-get '/bankey/account/1' do
-{
-   id: "1",
-   accountType: "Banking",
-   accountName: "Savings",
-   balance: 100.00
-}.to_json
-
 get '/bankey/profile/1' do
   {
       id: "1",
