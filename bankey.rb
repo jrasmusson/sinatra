fe1ba5a455cce937e38bcfdfcad1607632f571a8
Jonathan Rasmusson <rasmus4200@gmail.com>
2021-11-23 10:27:59 -0700

    All accounts

--- bankey.rb
+++ bankey.rb
@@ -9,15 +9,6 @@ get '/bankey/customer/1/accounts' do
   send_file 'bankey/accounts-1.json'
 end
 
-get '/bankey/account/1' do
-  {
-      id: "1",
-      accountType: "Banking",
-      accountName: "Basic Savings",
-      balance: 929466.23
-  }.to_json
-end
-
 get '/bankey/profile/1' do
   {
       id: "1",
