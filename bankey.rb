bcea2906d860a22cae430c4bfb28e0467760ea77
Jonathan Rasmusson <rasmus4200@gmail.com>
2021-12-02 05:46:41 -0700

    Multiple accounts

--- bankey.rb
+++ bankey.rb
@@ -5,11 +5,27 @@ before do
   content_type :json
 end
 
+get '/bankey/profile/1' do
+    send_file 'bankey/profile-1.json'
+end
+
+get '/bankey/profile/2' do
+    send_file 'bankey/profile-2.json'
+end
+
+get '/bankey/profile/3' do
+    send_file 'bankey/profile-3.json'
+end
+
 get '/bankey/profile/1/accounts' do
   send_file 'bankey/accounts-1.json'
 end
 
-get '/bankey/profile/1' do
-    send_file 'bankey/profile-1.json'
+get '/bankey/profile/2/accounts' do
+  send_file 'bankey/accounts-2.json'
+end
+
+get '/bankey/profile/3/accounts' do
+  send_file 'bankey/accounts-3.json'
 end
 
